be214e9a2cfb9e7ad128a77a476e8e3ae4f30391
vietpn <vietpnk53@gmail.com>
2015-10-11 20:00:17 +0700

    update

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workspaceYeomanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the workspaceYeomanApp
+ */
+angular.module('workspaceYeomanApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workspaceYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the workspaceYeomanApp
+ */
+angular.module('workspaceYeomanApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
